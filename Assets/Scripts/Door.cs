ec32828ce2372b413142b2f722b682723741c6f2
GreenHP <furkankarakus789@gmail.com>
2024-02-16 01:52:21 +0300

    Door SFX New Font
    - Some changes in door.cs
    - Added New Font And finished Nothing Room
    - Major changes in sandbox Furkan (door)

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -11,14 +10,20 @@ public class Door : MonoBehaviour
     [SerializeField] float playerDistance = 2.0f;
     [SerializeField] RandomWheelDecider buttonScript = null;
     [SerializeField] bool isLocked = true;
-    Quaternion openRotation;
-    bool isPlayerInDistance;
-    bool isOpen;
+    [SerializeField] AudioClip doorCloseSFX;
+    [SerializeField] AudioClip doorOpenSFX;
+    [SerializeField] AudioSource doorSource;
+
+    private Quaternion openRotation;
+    private Quaternion closedRotation;
+    private bool isPlayerInDistance;
+    private bool isOpen;
 
     private void Start()
     {
         doorText.enabled = false;
         openRotation = transform.rotation * Quaternion.Euler(0, openAngle, 0);
+        closedRotation = Quaternion.Euler(0, 0, 0);
     }
 
     private void Update()
@@ -29,51 +34,70 @@ public class Door : MonoBehaviour
         {
             OpenSlowly();
         }
-        else if (isOpen || !isLocked) 
+        else if (isOpen || !isLocked)
         {
-            KeepDoorOpenSequance();
+            CloseSlowly();
         }
-        
+
+        if (buttonScript == null)
+        {
+            ToggleDoorState();
+        }
     }
+    #region Mouse Input's
     private void OnMouseOver()
     {
-        if (!isOpen && isPlayerInDistance)
-        {
-            doorText.enabled = true;
-        }
-        else
-        {
-            doorText.enabled = false;
-        }
+        doorText.enabled = !isOpen && isPlayerInDistance;
     }
+
     private void OnMouseExit()
     {
-        if (!isOpen)
-        {
-            doorText.enabled = false;
-        }
+        doorText.enabled = !isOpen;
     }
 
     private void OnMouseDown()
     {
-        if (buttonScript)
+        if (buttonScript && isPlayerInDistance && buttonScript.SpinningFinished && !isOpen)
         {
-            if (isPlayerInDistance && buttonScript.SpinningFinished)
-            {
-                isOpen = true;
-                doorText.enabled = false;
-            }
+            ToggleDoorState();
         }
     }
-
+    #endregion
+    #region Open & Close Sequance's
     private void OpenSlowly()
     {
         transform.rotation = Quaternion.Lerp(transform.rotation, openRotation, Time.deltaTime * openSpeed);
     }
 
-    private void KeepDoorOpenSequance()
+    private void CloseSlowly()
     {
-        Quaternion closedRotation = Quaternion.Euler(0, 0, 0); 
         transform.rotation = Quaternion.Lerp(transform.rotation, closedRotation, Time.deltaTime * openSpeed);
+
+        if (Quaternion.Angle(transform.rotation, closedRotation) < 1.0f && isOpen)
+        {
+            isOpen = false;
+            PlayDoorSound(doorCloseSFX);
+        }
+    }
+    #endregion
+    private void ToggleDoorState()
+    {
+        isOpen = true;
+        doorText.enabled = false;
+
+        if (isOpen)
+        {
+            PlayDoorSound(doorOpenSFX);
+        }
+        else
+        {
+            PlayDoorSound(doorCloseSFX);
+        }
+    }
+
+    private void PlayDoorSound(AudioClip soundClip)
+    {
+        doorSource.clip = soundClip;
+        doorSource.Play();
     }
 }
